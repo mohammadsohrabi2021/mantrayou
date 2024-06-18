c6518a039e24f5721f8ed794bfe2e67b02889949
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-06-18 17:18:04 +0330

    create file dataMune done

---
+++ Data/DataMune.jsx
@@ -0,0 +1,11 @@
+const dataMune = [
+  { id: 1, title: "صفحه اصلی", href: "" },
+  { id: 2, title: "محصولات", href: "" },
+  { id: 3, title: "دسته‌بندی محصولات", href: "" },
+  { id: 4, title: "برندها", href: "" },
+  { id: 5, title: "درباره ما", href: "" },
+  { id: 6, title: "سوالات متداول", href: "" },
+  { id: 7, title: "تماس با ما ", href: "" },
+  { id: 8, title: "وبلاگ", href: "" },
+];
+export default dataMune;
