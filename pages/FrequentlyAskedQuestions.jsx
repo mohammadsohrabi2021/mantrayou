91d2a88ba5294e83270fe3af0f8ad700a8de2536
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-06-18 23:52:45 +0330

    create component route FrequentlyAskedQuestions done

---
+++ pages/FrequentlyAskedQuestions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FrequentlyAskedQuestions() {
+  return (
+    <div>
+      FrequentlyAskedQuestions
+    </div>
+  )
+}
+
+export default FrequentlyAskedQuestions
