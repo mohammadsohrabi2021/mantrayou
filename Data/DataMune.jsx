b6041cd4fea9a13b0bed3265baa0a5fa71e8b252
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-06-18 17:23:39 +0330

    change file dataMune done

--- Data/DataMune.jsx
+++ Data/DataMune.jsx
@@ -1,11 +1,11 @@
 const dataMune = [
-  { id: 1, title: "صفحه اصلی", href: "" },
-  { id: 2, title: "محصولات", href: "" },
-  { id: 3, title: "دسته‌بندی محصولات", href: "" },
-  { id: 4, title: "برندها", href: "" },
-  { id: 5, title: "درباره ما", href: "" },
-  { id: 6, title: "سوالات متداول", href: "" },
-  { id: 7, title: "تماس با ما ", href: "" },
-  { id: 8, title: "وبلاگ", href: "" },
+  { id: 1, title: "صفحه اصلی", href: "/" },
+  { id: 2, title: "محصولات", href: "/products" },
+  { id: 3, title: "دسته‌بندی محصولات", href: "/productsCategorization" },
+  { id: 4, title: "برندها", href: "/brands" },
+  { id: 5, title: "درباره ما", href: "/about-us" },
+  { id: 6, title: "سوالات متداول", href: "/FrequentlyAskedQuestions" },
+  { id: 7, title: "تماس با ما ", href: "/contact-us" },
+  { id: 8, title: "وبلاگ", href: "/blog" },
 ];
 export default dataMune;
