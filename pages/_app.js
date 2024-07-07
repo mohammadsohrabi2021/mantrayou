6676aa1c4ba82ec41a8041609523997f56e090f3
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-07-08 01:20:50 +0330

    update component _app.js done

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,24 @@ import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        style={{ width: "auto", fontSize: "16px" }}
+      />
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
