c52fe4d6d7892831eeb57c3d3144796d66caa301
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-07-09 02:54:30 +0330

    create  file data dataInput done

---
+++ Data/DataInputProfile.jsx
@@ -0,0 +1,34 @@
+export const dataInput = [
+  { id: 1, lable: "آدرس", name: "address_text", required: "آدرس الزامی است" },
+  {
+    id: 2,
+    lable: "نام استان",
+    name: "province_name",
+    required: "نام استان الزامی است",
+  },
+  {
+    id: 3,
+    lable: "نام شهر",
+    name: "city_name",
+    required: "نام شهر الزامی است",
+  },
+  {
+    id: 4,
+    lable: "کد پستی",
+    name: "postal_code",
+    required: "کد پستی الزامی است",
+  },
+  {
+    id: 5,
+    lable: "نام آدرس",
+    name: "address_name",
+    required: "نام آدرس الزامی است",
+  },
+  //   { id: 6, lable: "یادداشت‌های اضافی", name: "additional_notes",required: "" },
+];
+export const dataMuneItem = [
+  { id: 1, title: "نمای کلی حساب", value: "accountOverview" },
+  { id: 2, title: "آدرس های من", value: "myAddresses" },
+  { id: 3, title: "سفارشات", value: "orders" },
+  { id: 4, title: " کلوب مانترا", value: "mantraClub" },
+];
