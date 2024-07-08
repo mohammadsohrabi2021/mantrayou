e7b7879dcf84636285aded52e3a092570afa597c
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-07-09 02:53:50 +0330

    create  file data categories done

---
+++ Data/DataBannerCategories.jsx
@@ -0,0 +1,18 @@
+import cat1 from "../assets/images/cat1.jpg";
+import cat2 from "../assets/images/cat2.jpg";
+import cat3 from "../assets/images/cat3.jpg";
+import cat4 from "../assets/images/cat4.jpg";
+import cat5 from "../assets/images/cat5.jpg";
+import cat6 from "../assets/images/cat6.jpg";
+import cat7 from "../assets/images/cat7.webp";
+import cat8 from "../assets/images/cat8.webp";
+export const categories = [
+  { id: 1, title: "فروش تولد", imageUrl: cat1 },
+  { id: 2, title: "کتاب پرفروش", imageUrl: cat2 },
+  { id: 3, title: "نوهایتن", imageUrl: cat3 },
+  { id: 4, title: "گیک و زرق و برق دار", imageUrl: cat4 },
+  { id: 5, title: "موی سر", imageUrl: cat5 },
+  { id: 6, title: "آکنه", imageUrl: cat6 },
+  { id: 7, title: "انتخاب پائولا", imageUrl: cat7 },
+  { id: 8, title: "رنگ وای", imageUrl: cat8 },
+];
