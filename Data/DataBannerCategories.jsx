69f502e0fc2e2e62299e8ceef8abf0d7b1254283
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-07-12 02:43:43 +0330

    update file dataBannerCategory done

--- Data/DataBannerCategories.jsx
+++ Data/DataBannerCategories.jsx
@@ -6,6 +6,10 @@ import cat5 from "../assets/images/cat5.jpg";
 import cat6 from "../assets/images/cat6.jpg";
 import cat7 from "../assets/images/cat7.webp";
 import cat8 from "../assets/images/cat8.webp";
+import cat9 from "../assets/images/userBanaer4.webp";
+import cat10 from "../assets/images/userBanaer3.webp";
+import cat11 from "../assets/images/userBanaer2.webp";
+import cat12 from "../assets/images/sale-3.webp";
 export const categories = [
   { id: 1, title: "فروش تولد", imageUrl: cat1 },
   { id: 2, title: "کتاب پرفروش", imageUrl: cat2 },
@@ -16,3 +20,9 @@ export const categories = [
   { id: 7, title: "انتخاب پائولا", imageUrl: cat7 },
   { id: 8, title: "رنگ وای", imageUrl: cat8 },
 ];
+export const dataBannerCategory=[
+  { id: 1, title: "فروش", imageUrl: cat12 },
+  { id: 2, title: "چیز های خوب", imageUrl: cat11 },
+  { id: 3, title: "کتاب فروش", imageUrl: cat10 },
+  { id: 4, title: "بازدید های جدید", imageUrl: cat9 },
+]
