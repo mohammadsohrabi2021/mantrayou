d1901cdec33502178c6529c5acea8a50f1c265ee
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-07-12 02:42:54 +0330

    update accountApi done

--- pages/api/account/accountApi.js
+++ pages/api/account/accountApi.js
@@ -1,12 +1,64 @@
 export const getDataAccountUser = async (token) => {
-    const res = await fetch(`https://api.mantrayou.com/client/my_account/overview`, {
+  const res = await fetch(
+    `https://api.mantrayou.com/client/my_account/overview`,
+    {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       },
-    });
-    const resData = await res.json();
-    return resData;
-  
-  };
+    }
+  );
+  const resData = await res.json();
+  return resData;
+};
+export const getDataAccountAddressesUserId = async (token,dataUser) => {
+  console.log(dataUser?._id)
+  const res = await fetch(
+    `https://api.mantrayou.com/admin/accounts/addresses/${dataUser?._id}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const resData = await res.json();
+  return resData;
+};
+export const postAddressesUser = async (token,data,dataUser) => {
+  const res = await fetch(`https://api.mantrayou.com/admin/accounts/addresses/add/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      user_id: dataUser?._id,
+      address_text: data?.address_text,
+      province_name: data?.province_name,
+      city_name: data?.city_name,
+      postal_code: data?.postal_code,
+      address_name: data?.address_name,
+      additional_notes: data?.additional_notes
+    }),
+  });
+  const resData = await res.json();
+  return resData;
+};
+export const deletedAddressesUser = async (token,id) => {
+
+  const res = await fetch(`https://api.mantrayou.com/client/my_account/addresses/${id}/`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    
+  });
+  const resData = await res.json();
+  console.log(resData)
+  return resData;
+};
+
