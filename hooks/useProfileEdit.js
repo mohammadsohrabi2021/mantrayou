5f13fa71801f50a6cfce6ff157c0cb004dc6cccf
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-07-14 10:55:27 +0330

    create file hooke useProfileEdit  done

---
+++ hooks/useProfileEdit.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import Cookies from 'js-cookie';
+import { toast } from 'react-toastify';
+import { updateUserProfile } from '@/pages/api/account/accountApi';
+
+const useProfileEdit = (initialData, fetchUserData) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [firstName, setFirstName] = useState(initialData.firstName);
+  const [lastName, setLastName] = useState(initialData.lastName);
+  const [birthday, setBirthday] = useState(initialData.birthday);
+
+  const handleEditProfile = () => {
+    setIsEditing(true);
+  };
+
+  const handleProfileChange = async () => {
+    const token = Cookies.get("token");
+    const full_name = `${firstName}-${lastName}`;
+    const resData = await updateUserProfile(token,  full_name, birthday );
+    if (resData?.status) {
+      toast.success("اطلاعات با موفقیت به‌روزرسانی شد");
+      setIsEditing(false);
+      fetchUserData();  // فراخوانی تابع fetchUserData بعد از موفقیت آمیز بودن عملیات
+    } else {
+      toast.error("خطا در به‌روزرسانی اطلاعات");
+    }
+  };
+console.log(birthday)
+  return {
+    firstName,
+    lastName,
+    birthday,
+    isEditing,
+    setFirstName,
+    setLastName,
+    setBirthday,
+    handleEditProfile,
+    handleProfileChange
+  };
+};
+
+export default useProfileEdit;
