97769652dacc26eb21d6db7ac88b52d30f5a6a61
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-07-20 13:59:17 +0330

    update file homePageApi done

--- pages/api/homePage/homePageApi.js
+++ pages/api/homePage/homePageApi.js
@@ -1,12 +1,27 @@
-export const getAllProducts = async (item_page,page) => {
-    const res = await fetch(`https://api.mantrayou.com/client/products/?page=${page}&items_per_page=${item_page}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        // "Authorization": `Bearer ${token}`
-      },
-    });
-    const resData = await res.json();
-    return resData;
-  
-  };
+export const getAllProducts = async (items_per_page, page, filter_by = null) => {
+
+  let url = `https://api.mantrayou.com/client/products/?page=${page}&items_per_page=${items_per_page}`;
+  if (filter_by) {
+    url += `&filter_by=${filter_by}`;
+  }
+
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const resData = await res.json();
+  return resData;
+};
+
+export const getTotalProducts = async () => {
+  const res = await fetch(`https://api.mantrayou.com/client/products/total_count`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
+  return data;
+};
