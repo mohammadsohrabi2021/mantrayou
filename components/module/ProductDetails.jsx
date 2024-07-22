722ccc50eff3adb4c4b2334617e8b3077c87832f
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-07-22 16:17:35 +0330

    update component ProductDetails done

--- components/module/ProductDetails.jsx
+++ components/module/ProductDetails.jsx
@@ -1,51 +1,85 @@
-import React, { useState, useRef, useEffect } from "react";
-import {
-  Grid,
-  Typography,
-  Box,
-  Button,
-  Chip,
-  CircularProgress,
-  IconButton,
-} from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
+import { useDispatch, useSelector } from "react-redux";
+import { Box, Button, Chip, CircularProgress, Grid, IconButton, Typography } from "@mui/material";
 import Image from "next/image";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import baseImage from "../../assets/images/logoSite.png";
+import ColorSelector from './ColorSelector'; // Import the ColorSelector component
+import { addToCartMethod, updateCartQuantityMethod, removeFromCartMethod } from "../../redux/appSlice";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 const ProductDetails = ({ product }) => {
-  const [quantity, setQuantity] = useState(1);
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+  const [selectedColor, setSelectedColor] = useState(null);
+  const [quantity, setQuantity] = useState(1);
   const router = useRouter();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.app.cart);
+  const [mounted, setMounted] = useState(false);
   const thumbnailRef = useRef(null);
   const selectedImage = product?.images[selectedImageIndex];
 
   useEffect(() => {
-    scrollToThumbnail(selectedImageIndex);
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (selectedImageIndex !== null) {
+      scrollToThumbnail(selectedImageIndex);
+    }
   }, [selectedImageIndex]);
 
-  if (router.isFallback) {
-    return (
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        height="100vh"
-      >
-        <CircularProgress />
-      </Box>
-    );
-  }
-
   const handleAddToCart = () => {
-    // Logic for adding product to cart
+    if (product?.has_variations && selectedColor) {
+      const productToAdd = {
+        id: product._id,
+        name: product.name,
+        price: product.price,
+        price_wd: product.price_wd,
+        variation: { color: selectedColor },
+        main_image: product.main_image,
+        quantity: quantity,
+      };
+      dispatch(addToCartMethod(productToAdd));
+      setSelectedColor(product.variations[0]?.color);
+      setQuantity(1);
+      toast.success("محصول شما با موفقیت به سبد خرید اضافه شد");
+    } else if (!product.has_variations) {
+      const existingProduct = cart.find(item => item.id === product._id && !item.variation.color);
+      if (existingProduct) {
+        dispatch(updateCartQuantityMethod({ id: product._id, variation: {}, quantity: existingProduct.quantity + 1 }));
+      } else {
+        const productToAdd = {
+          id: product._id,
+          name: product.name,
+          price: product.price,
+          price_wd: product.price_wd,
+          variation: {},
+          main_image: product.main_image,
+          quantity: 1,
+        };
+        dispatch(addToCartMethod(productToAdd));
+        toast.success("محصول شما با موفقیت به سبد خرید اضافه شد");
+      }
+    }
+  };
+
+  const handleUpdateQuantity = (id, variation, newQuantity) => {
+    if (newQuantity === 0) {
+      dispatch(removeFromCartMethod({ id, variation }));
+    } else {
+      dispatch(updateCartQuantityMethod({ id, variation, quantity: newQuantity }));
+    }
   };
 
   const handlePreviousImage = () => {
     setSelectedImageIndex((prevIndex) => {
-      const newIndex =
-        prevIndex === 0 ? product.images.length - 1 : prevIndex - 1;
+      const newIndex = prevIndex === 0 ? product.images.length - 1 : prevIndex - 1;
       scrollToThumbnail(newIndex);
       return newIndex;
     });
@@ -53,8 +87,7 @@ const ProductDetails = ({ product }) => {
 
   const handleNextImage = () => {
     setSelectedImageIndex((prevIndex) => {
-      const newIndex =
-        prevIndex === product.images.length - 1 ? 0 : prevIndex + 1;
+      const newIndex = prevIndex === product.images.length - 1 ? 0 : prevIndex + 1;
       scrollToThumbnail(newIndex);
       return newIndex;
     });
@@ -64,10 +97,7 @@ const ProductDetails = ({ product }) => {
     const thumbnailList = thumbnailRef.current;
     if (thumbnailList) {
       const thumbnail = thumbnailList?.children[index];
-      const offset =
-        thumbnail?.offsetLeft -
-        thumbnailList?.offsetWidth / 2 +
-        thumbnail?.offsetWidth / 2;
+      const offset = thumbnail?.offsetLeft - thumbnailList?.offsetWidth / 2 + thumbnail?.offsetWidth / 2;
       thumbnailList.scrollTo({
         left: offset,
         behavior: "smooth",
@@ -75,91 +105,33 @@ const ProductDetails = ({ product }) => {
     }
   };
 
+  const productInCart = cart.find(item => item.id === product._id && !item.variation.color);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
-    <Grid
-      my={4}
-      display={"flex"}
-      flexDirection={{ xs: "column", md: "row-reverse" }}
-      justifyContent={"space-between"}
-      alignItems={{ md: "center" }}
-      width={'100%'}
-    >
+    <Grid my={4} display={"flex"} flexDirection={{ xs: "column", md: "row-reverse" }} justifyContent={"space-between"} alignItems={{ md: "center" }} width={'100%'}>
       <Grid item xs={12} sm={4} md={4}>
-        <Box
-          display="flex"
-          flexDirection={{ xs: "column", md: "row" }}
-          gap={2}
-          alignItems="center"
-          position="relative"
-        >
-          <Box
-            sx={{
-              width: { xs: "100%", sm: "100%", md: 500 },
-              height: { xs: 250, sm: 350, md: 500 },
-              position: "relative",
-              overflow: "hidden",
-            }}
-          >
+        <Box display="flex" flexDirection={{ xs: "column", md: "row" }} gap={2} alignItems="center" position="relative">
+          <Box sx={{ width: { xs: "100%", sm: "100%", md: 500 }, height: { xs: 250, sm: 350, md: 500 }, position: "relative", overflow: "hidden" }}>
             <Image
-              src={
-                selectedImage !== undefined
-                  ? `https://api.mantrayou.com/images/${selectedImage}`
-                  : `${baseImage?.src}`
-              }
+              src={selectedImage !== undefined ? `https://api.mantrayou.com/images/${selectedImage}` : `${baseImage?.src}`}
               alt={product?.name || 'Product Image'}
               layout="fill"
               objectFit="contain"
               quality={100}
               style={{ marginBottom: "16px" }}
             />
-            <IconButton
-              onClick={handlePreviousImage}
-              sx={{
-                position: "absolute",
-                top: "50%",
-                left: "10px",
-                transform: "translateY(-50%)",
-                color: "white",
-                backgroundColor: "rgba(0, 0, 0, 0.5)",
-                "&:hover": {
-                  backgroundColor: "rgba(0, 0, 0, 0.7)",
-                },
-              }}
-            >
+            <IconButton onClick={handlePreviousImage} sx={{ position: "absolute", top: "50%", left: "10px", transform: "translateY(-50%)", color: "white", backgroundColor: "rgba(0, 0, 0, 0.5)", "&:hover": { backgroundColor: "rgba(0, 0, 0, 0.7)", } }}>
               <ArrowBackIosIcon />
             </IconButton>
-            <IconButton
-              onClick={handleNextImage}
-              sx={{
-                position: "absolute",
-                top: "50%",
-                right: "10px",
-                transform: "translateY(-50%)",
-                color: "white",
-                backgroundColor: "rgba(0, 0, 0, 0.5)",
-                "&:hover": {
-                  backgroundColor: "rgba(0, 0, 0, 0.7)",
-                },
-              }}
-            >
+            <IconButton onClick={handleNextImage} sx={{ position: "absolute", top: "50%", right: "10px", transform: "translateY(-50%)", color: "white", backgroundColor: "rgba(0, 0, 0, 0.5)", "&:hover": { backgroundColor: "rgba(0, 0, 0, 0.7)", } }}>
               <ArrowForwardIosIcon />
             </IconButton>
           </Box>
-          <Box
-            ref={thumbnailRef}
-            display="flex"
-            flexDirection={{ xs: "row", md: "column" }}
-            gap={2}
-            maxHeight={{ xs: "100%", sm: "300px", md: "500px" }}
-            maxWidth={{ xs: "100%", sm: "500px" }}
-            overflow={"auto"}
-            sx={{
-              scrollbarWidth: "none" /* Firefox */,
-              "&::-webkit-scrollbar": {
-                display: "none" /* Chrome, Safari and Opera */,
-              },
-            }}
-          >
+          <Box ref={thumbnailRef} display="flex" flexDirection={{ xs: "row", md: "column" }} gap={2} maxHeight={{ xs: "100%", sm: "300px", md: "500px" }} maxWidth={{ xs: "100%", sm: "500px" }} overflow={"auto"} sx={{ scrollbarWidth: "none" /* Firefox */, "&::-webkit-scrollbar": { display: "none" /* Chrome, Safari and Opera */, } }}>
             {product?.images?.map((image, index) => (
               <Image
                 key={index}
@@ -168,33 +140,15 @@ const ProductDetails = ({ product }) => {
                 width={60}
                 height={60}
                 onClick={() => setSelectedImageIndex(index)}
-                style={{
-                  cursor: "pointer",
-                  border:
-                    selectedImageIndex === index ? "2px solid #000" : "none",
-                }}
+                style={{ cursor: "pointer", border: selectedImageIndex === index ? "2px solid #000" : "none", }}
               />
             ))}
           </Box>
         </Box>
       </Grid>
-      <Grid
-        item
-        xs={12}
-        sm={8}
-        md={8}
-        className="box-shadow"
-        my={4}
-        borderRadius={2}
-        p={{ xs: 2, sm: 4 }}
-        width={{ xs: '100%', md: '50%' }}
-      >
-        <Typography variant="h4" fontWeight="bold">
-          {product?.name}
-        </Typography>
-        <Typography variant="subtitle1" color="textSecondary">
-          {product?.brand}
-        </Typography>
+      <Grid item xs={12} sm={8} md={8} className="box-shadow" my={4} borderRadius={2} p={{ xs: 2, sm: 4 }} width={{ xs: '100%', md: '50%' }}>
+        <Typography variant="h4" fontWeight="bold">{product?.name}</Typography>
+        <Typography variant="subtitle1" color="textSecondary">{product?.brand}</Typography>
         <Box display="flex" alignItems="center" mt={2}>
           <Chip label="جدید" color="success" />
           {product?.sell_count > 0 && (
@@ -205,47 +159,49 @@ const ProductDetails = ({ product }) => {
             />
           )}
         </Box>
-        <Typography variant="h5" color="textSecondary" mt={2}>
-          {product?.price} تومان
-        </Typography>
+        <Typography variant="h5" color="textSecondary" mt={2}>{product?.price} تومان</Typography>
         {product?.price_discount && (
-          <Typography variant="body2" color="red">
-            {product?.price_wd} تومان (تخفیف {product?.price_discount}%)
-          </Typography>
+          <Typography variant="body2" color="red">{product?.price_wd} تومان (تخفیف {product?.price_discount}%)</Typography>
         )}
-        <Typography variant="body2" color="textSecondary" mt={1}>
-          مالیات و هزینه ارسال جداگانه محاسبه می‌شود
-        </Typography>
-        <Box mt={2}>
-          <Button variant="contained" color="primary" onClick={handleAddToCart}>
-            افزودن به سبد خرید
-          </Button>
-        </Box>
-        <Typography variant="body1" mt={2}>
-          {product?.description}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" mt={2}>
-          وزن: {product?.weight} گرم
-        </Typography>
-        <Typography variant="body2" color="textSecondary">
-          ابعاد: {product?.dimensions?.length}x{product?.dimensions?.width}x
-          {product?.dimensions?.height} سانتی‌متر
-        </Typography>
-        <Typography variant="body2" color="textSecondary">
-          مواد: {product?.material}
-        </Typography>
-        <Typography variant="body2" color="textSecondary">
-          موجودی: {product?.availability ? "موجود در انبار" : "ناموجود"}
-        </Typography>
-        {product?.tags?.map((tag, index) => (
-          <Chip
-            key={index}
-            label={tag}
-            variant="outlined"
-            style={{ marginRight: "4px", marginTop: "8px" }}
+        <Typography variant="body2" color="textSecondary" mt={1}>مالیات و هزینه ارسال جداگانه محاسبه می‌شود</Typography>
+        
+        {product?.variations && (
+          <ColorSelector
+            colors={product.variations.map(v => v.color)}
+            onColorSelect={setSelectedColor}
+            selectedColor={selectedColor}
+            quantity={quantity}
+            onQuantityChange={setQuantity}
           />
+        )}
+        
+        <Box mt={2}>
+          {(!product.has_variations && productInCart) ? (
+            <Box display="flex" alignItems="center">
+              <IconButton onClick={() => handleUpdateQuantity(product._id, {}, productInCart.quantity - 1)}>
+                <RemoveIcon />
+              </IconButton>
+              <Typography>{productInCart.quantity}</Typography>
+              <IconButton onClick={() => handleUpdateQuantity(product._id, {}, productInCart.quantity + 1)}>
+                <AddIcon />
+              </IconButton>
+            </Box>
+          ) : (
+            <Button variant="contained" color="primary" onClick={handleAddToCart} disabled={product.has_variations && !selectedColor}>
+              افزودن به سبد خرید
+            </Button>
+          )}
+        </Box>
+        <Typography variant="body1" mt={2}>{product?.description}</Typography>
+        <Typography variant="body2" color="textSecondary" mt={2}>وزن: {product?.weight} گرم</Typography>
+        <Typography variant="body2" color="textSecondary">ابعاد: {product?.dimensions?.length}x{product?.dimensions?.width}x{product?.dimensions?.height} سانتی‌متر</Typography>
+        <Typography variant="body2" color="textSecondary">مواد: {product?.material}</Typography>
+        <Typography variant="body2" color="textSecondary">موجودی: {product?.availability ? "موجود در انبار" : "ناموجود"}</Typography>
+        {product?.tags?.map((tag, index) => (
+          <Chip key={index} label={tag} variant="outlined" style={{ marginRight: "4px", marginTop: "8px" }} />
         ))}
       </Grid>
+      <ToastContainer />
     </Grid>
   );
 };
