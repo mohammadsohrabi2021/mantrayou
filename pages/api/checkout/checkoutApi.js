57db2d9009dd52633d0990c91db8923aac023fb1
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-08-06 13:37:46 +0330

    update component  file api checkoutApi done

--- pages/api/checkout/checkoutApi.js
+++ pages/api/checkout/checkoutApi.js
@@ -9,3 +9,22 @@ export const getDataCheckOutUser = async (token, dataUser) => {
   const resData = await res.json();
   return resData;
 };
+// فایل: api/addressApi.js
+
+const API_URL = 'https://api.mantrayou.com/client/cart/choose_address';
+
+export const selectUserAddress = async (addressId, token) => {
+  console.log(`${API_URL}/${addressId}`)
+  const response = await fetch(`${API_URL}/${addressId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error('Failed to select address');
+  }
+  return await response.json();
+};
+
