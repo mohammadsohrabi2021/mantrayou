fce2b9893868696f8e25f27acb8a6521582989a0
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-08-10 22:29:33 +0330

    update component index checkout done

--- pages/checkout/index.jsx
+++ pages/checkout/index.jsx
@@ -1,39 +1,37 @@
-import CheckoutPage from '@/components/template/CheckoutPage'
-import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { getDataCheckOutUser } from '../api/checkout/checkoutApi';
-import Cookies from 'js-cookie';
-import { Grid, Typography ,CircularProgress} from '@mui/material';
-import { saveCheckoutInfoMethod } from '@/redux/appSlice';
+import CheckoutPage from "@/components/template/CheckoutPage";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Grid, Typography, CircularProgress } from "@mui/material";
+import { fetchData } from "@/utils/fetchDataCheckOut";
+import Cookies from "js-cookie";
 
 function Checkout() {
-    const dispatch = useDispatch();
-    const [loading, setLoading] = useState(true);
-    const token = Cookies.get('token');
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getDataCheckOutUser(token);
-        dispatch(saveCheckoutInfoMethod(data)); // فرض بر این است که API داده‌های سبد خرید را بر می‌گرداند
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setLoading(false);
-      }
-    };
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const token = Cookies.get("token");
 
-    fetchData();
+  useEffect(() => {
+    fetchData(dispatch, setLoading);
   }, [dispatch, token]);
 
   if (loading) {
-    return <Grid display={'flex'}gap={1} height={'100vh'} justifyContent={'center'} alignItems={'center'}>
-    <Typography fontFamily={'iran-sans'}>درحال دریافت اطلاعات  لطفا صبور باشید...</Typography>
-     <CircularProgress />
-   </Grid>;
+    return (
+      <Grid
+        display={"flex"}
+        gap={1}
+        height={"100vh"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Typography fontFamily={"iran-sans"}>
+          درحال دریافت اطلاعات لطفا صبور باشید...
+        </Typography>
+        <CircularProgress />
+      </Grid>
+    );
   }
-  return (
-  <CheckoutPage/>
-  )
+
+  return <CheckoutPage />;
 }
 
-export default Checkout
+export default Checkout;
