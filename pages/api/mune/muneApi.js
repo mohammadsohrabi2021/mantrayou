9db4deeab55e9a17288fe79d6b4ed6a13f42e92f
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-08-28 01:12:23 +0330

    update component file muneApi done

--- pages/api/mune/muneApi.js
+++ pages/api/mune/muneApi.js
@@ -10,3 +10,37 @@ export const getAllListsCategories = async (level) => {
     return resData;
   
   };
+  // categoryApi.js
+const BASE_URL = 'https://api.mantrayou.com/client/categories/childs/';
+
+export const fetchCategory = async (categoryId) => {
+  console.log(categoryId)
+  try {
+    const response = await fetch(`${BASE_URL}${categoryId}`);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching category data:", error);
+    return null;
+  }
+};
+export const fetchCategoryInfo = async (categoryId) => {
+  try {
+    const response = await fetch(`https://api.mantrayou.com/client/categories/${categoryId}/info`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching category info:', error);
+    throw error;
+  }
+};
